f08918d2e2c8b873b6377d38c6aef0add84c5dce
Seisuke Fukuda <razerrmc163@gmail.com>
2024-07-09 14:03:39 +0900

    add bookmark on list function finished

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,24 @@
 class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list_id = @list.id
+    @bookmark.save
+    redirect_to list_path(@list)
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
 end
