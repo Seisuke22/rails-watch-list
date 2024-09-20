ae5f91f852ddfcf51dac3f5d33f787cafaae33c2
Seisuke Fukuda <razerrmc163@gmail.com>
2024-09-20 23:19:49 +0900

    bookmark movies through show page

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,20 +1,34 @@
 class BookmarksController < ApplicationController
   def new
     @bookmark = Bookmark.new
-
-    if params[:list_id]
-      @list = List.find(params[:list_id])
-    end
-
-    if params[:movie_id]
-      @movie = Movie.find(params[:movie_id])
-    end
+    @movie = Movie.find(params[:movie_id]) if params[:movie_id]
+    @list = List.find(params[:list_id]) if params[:list_id]
   end
 
   def create
-    @bookmark = Bookmark.new(bookmark_params)
-    @list = List.find(params[:list_id])
-    @bookmark.list_id = @list.id
+    @bookmark = current_user.bookmarks.new(bookmark_params)
+
+    # Assign movie if provided
+    if params[:movie_id]
+      @movie = Movie.find(params[:movie_id])
+      @bookmark.movie = @movie
+    end
+
+    # Check and assign list if provided
+    if bookmark_params[:list_id].present?
+      @list = List.find_by(id: bookmark_params[:list_id])
+      if @list.nil?
+        flash[:alert] = "List not found."
+        redirect_to lists_path and return
+      else
+        @bookmark.list = @list
+      end
+    else
+      flash[:alert] = "Please select a list to add the bookmark."
+      redirect_to lists_path and return
+    end
+
+    # Attempt to save the bookmark
     if @bookmark.save
       flash[:notice] = "Bookmark was successfully created."
       redirect_to list_path(@list)
@@ -24,16 +38,12 @@ class BookmarksController < ApplicationController
     end
   end
 
-  def destroy
-    @bookmark = Bookmark.find(params[:id])
-    @bookmark.destroy
-    flash[:notice] = "Bookmark was successfully deleted."
-    redirect_to bookmark_path(@bookmark)
-  end
 
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment, :movie_id)
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
   end
+
+
 end
