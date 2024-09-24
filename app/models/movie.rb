deab3874a5b1796ddce5a6a4058328ccd27d8459
Seisuke Fukuda <razerrmc163@gmail.com>
2024-09-24 16:03:08 +0900

    few testing in model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,5 +4,5 @@ class Movie < ApplicationRecord
   belongs_to :user
 
   validates :title, uniqueness: true
-  validates :actor_name, :actor_image, :release_year, :overview, :poster_url, :trailer_url, presence: true
+  validates :title, :actor_name, :actor_image, :release_year, :overview, :poster_url, :trailer_url, presence: true
 end
