80e57bf009a45f7038d9623626909685e3db4186
Seisuke Fukuda <razerrmc163@gmail.com>
2024-10-04 14:27:11 +0900

    factory bot added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
-# frozen_string_literal: true
-
+# Controller for USER
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
